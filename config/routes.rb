470829630db342c36fa585099b3f89b1ce88f00a
hiroki2020 <n64.shibata.hiroki@gmail.com>
2019-12-09 12:57:37 +0900

    何かを書いてみるページの編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "item#index"
   resources :item, only:[:index, :create, :new]
+  
   get 'item/try', to: 'item#try' 
+  # 何かを書いてみる為のページ用のルーティング
+  
 end
