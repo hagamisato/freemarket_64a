891ac3d33916caec985921ee547b5bf54f68f48b
64minato <57395252+64minato@users.noreply.github.com>
2019-12-10 20:48:11 +0900

    routes set

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "item#index"
   resources :item, only:[:index, :create, :new, :show]
+  resources :signup do
+    collection do
+      post :top
+      post :reg
+      post :tell
+      post :add
+      post :pay
+      post :finish
+    end
+  end
   
   get 'item/try', to: 'item#try' 
   # 何かを書いてみる為のページ用のルーティング
