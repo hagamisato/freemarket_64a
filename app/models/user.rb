4ad48c3830a05dcab5f04fbf1638fe8a439d99d6
64minato <57395252+64minato@users.noreply.github.com>
2019-12-11 20:07:21 +0900

    readme2

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,18 +9,5 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         kanji = /\A[一-龥]+\z/
-         kana = /\A([ァ-ン]|ー)+\z/
-         year_month_day = /\A\d{4}-\d{2}-\d{2}\z/
-
-  validates :nickname, presence: true, length: { maximum: 15 },profanity_filter: true
-  validates :f_name_kanji, presence: true, length: { maximum: 15 }, format: { with: kanji }
-  validates :l_name_kanji, presence: true, length: { maximum: 15 }, format: { with: kanji }
-  validates :f_name_kana, presence: true, length: { maximum: 15 }, format: { with: kana }
-  validates :l_name_kana, presence: true, length: { maximum: 15 }, format: { with: kana }
-  validates :birthday, presence: true, format: { with: year_month_day }
-  validates :password,presence: true
-  validates :password_confirmation,presence: true
-       
 
 end
