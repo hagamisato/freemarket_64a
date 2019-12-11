e3c171ee248002e2ac1b5ab6d509871eb56da5ec
hiroki2020 <n64.shibata.hiroki@gmail.com>
2019-12-11 12:10:46 +0900

    imagesテーブルの作成
    item_idのカラム追加の記述はコメントアウトした。
    →rails db:migrateを実行したらエラーがでたため。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_10_072208) do
+ActiveRecord::Schema.define(version: 2019_12_11_025815) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2019_12_10_072208) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+  end
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
