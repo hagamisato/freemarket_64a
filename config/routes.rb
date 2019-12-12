a4b337b1e2161836bb7b3bb1e5d8f8bb3c4d33fa
hiroki2020 <n64.shibata.hiroki@gmail.com>
2019-12-12 18:06:00 +0900

    ルーティングの調整

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "item#index"
-  resources :item, only:[:index, :create, :new, :show]
-  
+  resources :item, only:[:index,:show]
+  resources :items, only:[:new, :create]
 end
