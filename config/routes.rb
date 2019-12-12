fe65aee595a9e50c8e65349f15cef298578ff4e0
hagamisato <n64.haga.misato@gmail.com>
2019-12-12 20:27:22 +0900

    payjp実装（ユーザーログイン前）

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'credit_card/new'
+  get 'credit_card/show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "item#index"
@@ -11,6 +13,14 @@ Rails.application.routes.draw do
       get :add
       get :pay
       get :finish
+    end
+  end
+
+  resources :credit_card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'credit_card#show'
+      post 'pay', to: 'credit_card#pay'
+      post 'delete', to: 'credit_card#delete'
     end
   end
   
--- db/schema.rb
+++ db/schema.rb
@@ -20,11 +20,9 @@ ActiveRecord::Schema.define(version: 2019_12_10_090957) do
   end
 
   create_table "credit_cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "credit_number", null: false
-    t.integer "limit_month", null: false
-    t.integer "limit_year", null: false
-    t.integer "security_number", null: false
     t.bigint "user_id", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_credit_cards_on_user_id"
@@ -50,5 +48,4 @@ ActiveRecord::Schema.define(version: 2019_12_10_090957) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "credit_cards", "users"
 end
