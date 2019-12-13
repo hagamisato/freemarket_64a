e0cafa29ea2f6c7eaf0cda5dec9781641181f7e9
hiroki2020 <n64.shibata.hiroki@gmail.com>
2019-12-13 20:07:27 +0900

    imagesとcategoryiesモデルに関するattributesの追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   has_many :images 
-  belongs_to :categories
-  # accepts_nested_attributes_for :images, :categories
+  has_many :categories
+  accepts_nested_attributes_for :images 
+  accepts_nested_attributes_for :categories
   attr_accessor :imageobject
 end
