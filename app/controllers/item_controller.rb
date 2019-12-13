f70e833152ff95bee34334b14c2bd43624cd29f8
hiroki2020 <n64.shibata.hiroki@gmail.com>
2019-12-13 16:11:42 +0900

    コントローラーの編集
    itemコントローラーから不要な記述を削除
    itemsコントローラーに必要な記述を追加

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -1,14 +1,4 @@
 class ItemController < ApplicationController
   def index
   end
-
-  def create
-    @item.save
-  end
-
-  def new
-    @item = Item.new
-  end
-
-
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,15 +2,16 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params) 
     @item.save
+    redirect_to '/'
   end
 
   def new
     @item = Item.new
-    @item.images.build
+    # @item.build
   end
 
   private
-  def item_params
-    params.require(:item).permit(:name, :explain, :category_id, :state, :postage, :shipping_area, :shipping_date, :price, images_attributes:[:id, :image])
-  end
+    def item_params
+      params.require(:item).permit(:image, :name, :explain, :state, :postage, :shipping_area, :shipping_date, :price)
+    end
 end
