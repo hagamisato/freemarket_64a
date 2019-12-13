01e94af0a765b0c449c4b146f9a4e2dc0027e6cc
hiroki2020 <n64.shibata.hiroki@gmail.com>
2019-12-13 18:43:38 +0900

    画像をアップロードするための記述
    uploading関数は、
    https://joppot.info/2014/05/28/1476
    を参考にしました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,16 @@
+require 'Kconv'
 class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params) 
-    @item.save
-    redirect_to '/'
+    image = item_params[:imageobject]
+    image_name = image.original_filename
+    @item.image = image.original_filename
+    result = uploading(image, image_name)
+    if @item.save
+      redirect_to '/'
+    else
+      redirect_to '/items/new'
+    end
   end
 
   def new
@@ -12,6 +20,24 @@ class ItemsController < ApplicationController
 
   private
     def item_params
-      params.require(:item).permit(:image, :name, :explain, :state, :postage, :shipping_area, :shipping_date, :price)
+      params.require(:item).permit(:imageobject, :name, :explain, :state, :postage, :shipping_area, :shipping_date, :price)
+    end
+
+    def uploading(img_object,image_name)
+      ext = image_name[image_name.rindex('.') + 1, 4].downcase
+      perms = ['.jpg', '.jpeg', '.gif', '.png']
+      if !perms.include?(File.extname(image_name).downcase)
+        result = 'アップロードできるのは画像ファイルのみです。'
+      elsif img_object.size > 4.megabyte
+        result = 'ファイルサイズは4MBまでです。'
+      else
+        File.open("public/#{image_name.toutf8}", 'wb') { |f| f.write(img_object.read) }
+        result = "success"
+      end
+      return result
+    end
+
+    def deleteimg(image_name)
+      File.unlink "public/"+image_name.toutf8
     end
 end
