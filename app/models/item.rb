1de32a3e4a7215c9a218966913e79691d2edace3
hiroki2020 <n64.shibata.hiroki@gmail.com>
2019-12-14 17:58:09 +0900

    imageに関するアソシエーションをコメントアウト

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
-  has_many :images 
+  # has_many :images 
   has_many :categories
-  accepts_nested_attributes_for :images 
+  # accepts_nested_attributes_for :images 
   accepts_nested_attributes_for :categories
-  attr_accessor :imageobject
+  # attr_accessor :imageobject
 end
