bc7bcd8de606bbaa6a65130093298e6bcf087cce
hiroki2020 <n64.shibata.hiroki@gmail.com>
2019-12-16 15:05:51 +0900

    ルーティングを変更した

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "item#index"
-  resources :items, only:[:new, :create]
-  resources :item, only:[:index, :create, :new, :show]
+  resources :items, only:[:new, :create, :show]
+  resources :item, only:[:index]
   resources :signup, only: [:index, :create] do
     collection do
       get :top
@@ -14,8 +14,5 @@ Rails.application.routes.draw do
       get :finish
     end
   end
-  
-  get 'item/try', to: 'item#try' 
-  # 何かを書いてみる為のページ用のルーティング
-  
+
 end
