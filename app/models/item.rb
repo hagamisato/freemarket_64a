ca558df487ebd720d557d1ce84e4776b80386df6
hiroki2020 <n64.shibata.hiroki@gmail.com>
2019-12-16 17:05:40 +0900

    バリデーションをかけようとした

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,6 @@ class Item < ApplicationRecord
   has_many :categories
   accepts_nested_attributes_for :images
   accepts_nested_attributes_for :categories
+  validates :name, {presence: true, length: {maximum: 40}}
   # mount_uploader :image, ImageUploader
 end
