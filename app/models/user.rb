d8a9d67da501ab5fa9eb902277b389c9de41c7b5
64minato <57395252+64minato@users.noreply.github.com>
2019-12-16 14:31:50 +0900

    DBコメントアウト削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   has_many :user_items
   has_one :address
   accepts_nested_attributes_for :address
-  # has_one :credit_card
-  # accepts_nested_attributes_for :credit_card
+ 
+
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
