732c3982db3cbb8fabca8b467cdb4f2b1e414aca
hagamisato <n64.haga.misato@gmail.com>
2019-12-17 12:23:08 +0900

    購入確認ページ完了

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,8 @@
+class PurchasesController < ApplicationController
+  def index
+    # @item = Item.find(params[:id])
+  end
+
+  def done
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  get 'purchases/index'
+  get 'purchases/done'
   devise_for :users
   root "items#index"
   resources :items, only:[:new, :create, :show]
   resources :item, only:[:index, :create, :new]
   resources :signups, only: [:index, :create] do
-
     collection do
       get :top
       get :reg
@@ -22,4 +23,11 @@ Rails.application.routes.draw do
       get :logout
     end
   end
+
+  resources :purchases, only: :index do
+    collection do
+    get :done
+    end
+  end
+  
 end
