fe6a472ee67fa4b8871d4cdf29ece602b9d542fa
hagamisato <n64.haga.misato@gmail.com>
2019-12-19 10:24:23 +0900

    修正where→findby書換

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -40,7 +40,7 @@ class CardsController < ApplicationController
   end
 
   def show #Cardのデータpayjpに送り情報を取り出します
-    card = Card.where(user_id: current_user.id).first
+    card = Card.find_by(user_id: current_user.id)
     if card.blank?
       redirect_to action: "new" 
     else
