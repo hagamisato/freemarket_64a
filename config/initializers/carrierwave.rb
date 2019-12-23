7da36b342aa5a4b25106881eab0facb9cfdbb8a5
hiroki2020 <n64.shibata.hiroki@gmail.com>
2019-12-23 16:38:31 +0900

    アップロード先をfogに設定した

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,21 +1,19 @@
 require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
-# require 'carrierwave/storage/fog'
+require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
-  # if Rails.env.development? || Rails.env.test?
-    config.storage = :file
-  # elsif Rails.env.production? 
-  #   config.storage = :fog
-  #   config.fog_provider = 'fog/aws'
-  #   config.fog_credentials = {
-  #     provider: 'AWS',
-  #     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-  #     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-  #     region: 'ap-northeast-1'
-  #   }
+
+CarrierWave.configure do |config| 
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+      region: 'S3-ap-northeast-1'
+    }
+end
     
-  #   config.fog_directory  = '64ateam'
-  #   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/64ateam'
-  # end
+  config.fog_directory  = 'freemarket64'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket64'
 end
