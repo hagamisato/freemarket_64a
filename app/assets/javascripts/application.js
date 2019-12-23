04c6fffd90386af96c4645d3fddb1e03a4c564c4
hagamisato <n64.haga.misato@gmail.com>
2019-12-23 12:17:25 +0900

    slick導入

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,14 @@
 //= require turbolinks
 //= require_tree .
 
+$(function() {
+  $('.main__header').slick({
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 5000,
+  });
+  $('.iconbox').on('click', function(e) {
+    e.preventDefault();
+    $('.main__header').slick('goTo', $(this).index());
+  });
+});
