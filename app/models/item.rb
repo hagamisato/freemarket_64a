e221488469f14cd8f2dca75c2b9f39526394f55b
hiroki2020 <n64.shibata.hiroki@gmail.com>
2019-12-24 12:47:44 +0900

    元の状態に戻した

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,12 @@
 class Item < ApplicationRecord
   has_many :images, dependent: :destroy
   has_many :categories, dependent: :destroy
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
   accepts_nested_attributes_for :images
   accepts_nested_attributes_for :categories
   validates :name, {presence: true, length: {maximum: 40}}
   has_many :users, through: :user_items
+  # extend ActiveHash::Associations::ActiveRecordExtensions
+  # belongs_to_active_hash :prefecture
+  # active_hashが上手く行かなかったのでコメントアウト
 end
